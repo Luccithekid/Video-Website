a48d31fefa6213b4e10249fd115b343bbd427401
Luccithekid <126lpetito@frhsd.com>
2025-10-21 08:32:35 -0400

    done

--- js/sandbox.js
+++
@@ -1,6 +0,0 @@
-	function selectVideo(idx) {
-			const thumbs = document.querySelectorAll('.sidebar-thumb');
-			thumbs.forEach((t, i) => t.classList.toggle('selected', i === idx));
-			const labels = ["Main Video", "Video 2", "Video 3", "Video 4"];
-			document.getElementById('mainVideo').innerHTML = `<div class='main-video-label'>${labels[idx]}</div><span>400 x 225 px</span>`;
-		}
